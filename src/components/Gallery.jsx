29cab53ac1db90cc6480518a32a9f5b2dbf55eae
itzmegelo <maldora.pojas.angelo@gmail.com>
2025-10-14 14:44:31 +0800

    Improve Ui

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,7 @@
 // components/Gallery.jsx
 import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const images = [
   "/images/catgirl1.jpg",
@@ -13,19 +15,22 @@ const images = [
 const Gallery = () => {
   return (
     <section className="py-12 px-4 md:px-12 bg-gray-100 dark:bg-gray-900 w-full">
-      <h2 className="text-6xl font-bold text-gray-900 dark:text-white mb-8 text-center">
+      <h1 className="text-6xl md:text-8xl font-black mb-6 leading-tight text-gray-900 dark:text-white">
         <span className="text-[#6366f1]">Anime</span> Gallery
-      </h2>
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {images.map((src, index) => (
           <div
             key={index}
             className="overflow-hidden rounded-lg shadow-lg hover:scale-105 transform transition duration-300"
           >
-            <img
+            <LazyLoadImage
               src={src}
               alt={`Gallery ${index + 1}`}
-              className="w-full h-70 object-cover"
+              className=" object-cover"
+              effect="blur" // adds smooth blur placeholder
+              width="100%"
+              height="100%"
             />
           </div>
         ))}
