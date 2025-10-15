10a126cdc19009b7b440aaad831b701d8c4b37ee
itzmegelo <maldora.pojas.angelo@gmail.com>
2025-10-15 17:19:20 +0800

    Improve Ui

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({ theme, toggleTheme }) {
 
   const navItems = [
     { name: "Home", href: "/" },
-    { name: "Winners", href: "/generate" },
+    { name: "Winners", href: "/winners" },
     { name: "About Us", href: "/aboutus" },
   ]
 
